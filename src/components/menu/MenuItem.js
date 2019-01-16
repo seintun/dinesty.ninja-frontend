d2b087d5432e83d81ca5b3110c74b0a03125b26a
seintun <seintun.edu@gmail.com>
2019-01-15 17:15:32 -0800

    Added Card to render individual menuItem with clickable button

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -1,27 +1,50 @@
 import React, { Component } from "react";
-import { Container, Content, Accordion } from "native-base";
+import { ScrollView, Image } from 'react-native'
+import { Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body } from 'native-base';
 
 export default class MenuItem extends Component {
-  state = { items: [] }
   componentDidMount() {
     this.setState({ items: this.props.menuItems.map(item => {
       return {
         title: item.name,
-        content: item.description
+        content: `Price: $${(item.price/100).toFixed(2)}\nDescription: ${item.description}`
       }
     })})
   }
+  renderItem() {
+    return this.props.menuItems.map(item =>
+      <Content>
+        <Card>
+          <CardItem>
+            <Left>
+              <Thumbnail source={{uri: `https://source.unsplash.com/${item.price+1}x900/?food,dine`}} />
+              <Body>
+                <Text>{item.name}</Text>
+                <Text note>${(item.price/100).toFixed(2)}</Text>
+              </Body>
+            </Left>
+          </CardItem>
+          <CardItem cardBody>
+            <Text style={{margin: 20}}>{item.description}</Text>
+          </CardItem>
+          <CardItem cardBody>
+            <Image source={{uri: `https://source.unsplash.com/${item.price}x900/?food,dine`}} style={{height: 200, width: null, flex: 1}}/>
+          </CardItem>
+          <CardItem>
+            <Button style={{flex: 1}}>
+              <Icon active name="cart" />
+              <Text>Add to Reservation</Text>
+            </Button>
+          </CardItem>
+        </Card>
+      </Content>
+    );
+  }
   render() {
     return (
-      <Container>
-        <Content padder>
-          <Accordion
-            dataArray={this.state.items}
-            headerStyle={{ backgroundColor: "orange" }}
-            contentStyle={{ backgroundColor: "#ddecf8" }}
-          />
-        </Content>
-      </Container>
+      <ScrollView>
+        {this.renderItem()}
+      </ScrollView>
     );
   }
 }
